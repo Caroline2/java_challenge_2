663d4c1ae69fd14111678ee168113725d6c47d2e
Caroline2 <kflute@seznam.cz>
2021-12-09 10:27:59 +0100

    methods declaration

--- Messenger.java
+++ Messenger.java
@@ -2,6 +2,8 @@ package com.example.javachallenge2;
 
 import java.util.Scanner;
 
+import javax.naming.ldap.ControlFactory;
+
 public class Messenger {
     private String owner;
 
@@ -13,6 +15,10 @@ public class Messenger {
 
     public void greet(){
         System.out.println("Welcome to Your messenger, " + getOwner() + "!");
+        dropDownList();
+    }
+
+    public void goBack(){
         dropDownList();
     }
 
@@ -52,17 +58,104 @@ public class Messenger {
     }
 
     public void manageContacts(){
-        System.out.println("1. Show all contacts");
-        System.out.println("2. Add a new contact");
-        System.out.println("3. Search a contact");
-        System.out.println("4. Delete a contact");
-        System.out.println("5. Go back to the previous menu");
+        int result = 0;
+
+        do{
+            System.out.println("Which action do You want to proceed?");
+
+            System.out.println("1. Show all contacts");
+            System.out.println("2. Add a new contact");
+            System.out.println("3. Search a contact");
+            System.out.println("4. Delete a contact");
+            System.out.println("5. Go back to the previous menu");
+
+            char contactsAction = sc.next().charAt(0);
+            System.out.println("You chose "+ contactsAction + "!");
+            result = checkContact(contactsAction);
+
+        }while(result == 1);
+    }
+
+    public int checkContact(char contactsAction){
+        int chosen = 1;
+
+        switch (contactsAction){
+            case '1': chosen = 0;
+                showContacts();
+                break;
+            case '2': chosen = 0;
+                addContact();
+                break;
+            case '3': chosen = 0;
+                searchContact();
+                break;
+            case '4': chosen = 0;
+                deleteContact();
+                break;
+            case '5': chosen = 0;
+                goBack();
+                break;
+            default: chosen = 1;            
+        }
+        return chosen;
+    }
+
+    public void showContacts(){
+    
+    }
+
+    public void addContact(){
+
+    }
+
+    public void searchContact(){
+
+    }
+
+    public void deleteContact(){
+
     }
 
     public void messages(){
-        System.out.println("1. See the list of all messages");
-        System.out.println("2. Send a new message");
-        System.out.println("3. Go back to the previous menu");
+        int sure = 0;
+        do{
+            System.out.println("Which action do You want to proceed?");
+
+            System.out.println("1. See the list of all messages");
+            System.out.println("2. Send a new message");
+            System.out.println("3. Go back to the previous menu");
+
+            char messagesAction = sc.next().charAt(0);
+            System.out.println("You chose "+ messagesAction + "!");
+            sure = checkMessage(messagesAction);
+
+        }while(sure == 1);
+    }
+
+    public int checkMessage(char messagesAction){
+        int chosen = 1;
+
+        switch (messagesAction){
+            case '1': chosen = 0;
+                showMessages();
+                break;
+            case '2': chosen = 0;
+                newMessage();
+                break;
+            case '3': chosen = 0;
+                goBack();
+                break;
+            default: chosen = 1;            
+        }
+        return chosen;
+    }
+
+    public void showMessages(){
+
+    }
+
+    public void newMessage(){
+
     }
 
     public void quit(){
